403537dd0d442cd3fee4008b774d6c2de74d9edf
Bernardo Bertante <bernardo.sctp@gmail.com>
2023-11-30 23:51:24 -0300

    CRUD simples completo, paginas estilisadas

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# ProjetoApi1
-Um projeto no qual minha motivação para cria-lô foi o desafio de trilhas Back Frontend do PS de Trainees da CompJR
+# Projeto: Cadastro de Clientes (Loja de Ração Simples)
+
+- Um projeto no qual minha motivação para cria-lô foi o desafio de trilhas Back Frontend do PS de Trainees da CompJR
+
+# Introdução ao Projeto:
+
+- Esse projeto envolve a criação de uma API com CRUD completo. O intruito é que o usuário tenha acesso a 3 páginas: a inicial, acessada pela rota "/", nessa aba são encontrados apenas os nomes dos clientes já cadastrados, acesso para a aba de novos cadastros e a visualização individual de cada usuário!
+
+- A 2 aba consiste no preenchimento de dados para o cadastro de um novo cliente, cada campo exceto o débito possui algum tipo de verificação (rota "/register")!
+
+- Já a última aba confere acesso a todas as informações de um indivíduo individualmente, podendo retornar a página principal por meio de um clieque na logo que está na Nav, editar por meio de um clique no icon de caneta, e excluir o cliente por meio do icon de lixeira (rota "/visualiser")!
+
+# Como Rodar?
+
+Em uma IDE, como o VSCOde, abra os arquivos projeto e o terminal do index.js! Logo, digite npm init -y depois disso npm install nodemon express mongoose ejs dotenv depois disso, vá até o arquivo de nome: package.json na parte de scripts, adcione mais um: "start": "nodemon ./index.js localhost 3000" exatamente assim! Lembre também que o arquivo: .env.example contém o necessário para você se conectar com SUA senha ao seu banco de dados! Crie um file: .env cole lá sua BD_URI = ! Por fim, no terminal, rode o comando npm start e abra em um navegador o localhost:3000 !Tá pronto o sorvetinho!
+
+# Tecnologias usadas:
+
+- Node.js
+- Express
+- MongoDB (Banco de Dados/Mongoose)
+- EJS
+- Dotenv?
+
+# Créditos
+
+- Deus
+- Minha namorada
+- Thiago
+- Matheus
--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 const Client = require("./models/Client");
-const clientRoutes = require("./routes/clientRoutes");
+let _client = null;
+let clientDel = null;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-app.use("/create", clientRoutes);
 
 connectToDB();
 
@@ -20,8 +20,54 @@ app.get("/", async (req, res) => {
     res.render("index", { clients });
 });
 
-app.get("/register", (req, res)=> {
+app.get("/register", (req, res) => {
     res.render("register");
 }); 
+
+app.get("/visualiser/:id", async (req, res) => {
+    const clientId = req.params.id;
+
+    try {
+        const client = await Client.findById(clientId);
+        
+        if (!client) {
+            return res.status(404).send('Cliente não encontrado');
+        }
+
+        res.render("clientVisualiser", { client, _client: null });
+    } catch (err) {
+        res.status(500).send('Erro ao buscar o cliente');
+    }
+});
+
+app.post("/create", async (req, res) => {
+    const client = req.body;
+    await Client.create(client);
+    res.redirect("/") ;
+});
+
+app.get("/edit/:id", async (req, res) => {
+    const clientId = req.params.id;
+
+    try {
+        _client = await Client.findById(clientId)
+        const client = await Client.findById(clientId);
+        res.render("clientVisualiser", { client, _client });
+    } catch (err) {
+        res.status(500).send('Erro ao buscar o cliente');
+    }
+});
+
+app.post("/update/:id", async (req, res) => {
+    const newClient = req.body;
+    await Client.updateOne({ _id: req.params.id }, newClient);
+    res.redirect("/visualiser/" + req.params.id);
+});
+
+app.get("/delete/:id", async (req, res) => {
+    await Client.deleteOne({ _id: req.params.id });
+    res.redirect("/");
+});
+
 
 app.listen(port, () => console.log(`Esse servidor esta rodando em http://localhost:${port}`));
